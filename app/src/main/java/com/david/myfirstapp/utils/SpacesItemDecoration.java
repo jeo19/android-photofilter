af6d4e301fe57331abfb5b25613da53f0682f294
jeo19 <terrakkk@outlook.com>
2020-04-05 22:14:43 +0800

    style:Building Instagram like interface

---
+++ app/src/main/java/com/david/myfirstapp/utils/SpacesItemDecoration.java
@@ -0,0 +1,27 @@
+package com.david.myfirstapp.utils;
+import android.graphics.Rect;
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View;
+
+/**
+ * Created by Jeo on 05/04/20.
+ */
+
+public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+    private int space;
+
+    public SpacesItemDecoration(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        if (parent.getChildAdapterPosition(view) == state.getItemCount() - 1) {
+            outRect.left = space;
+            outRect.right = 0;
+        }else{
+            outRect.right = space;
+            outRect.left = 0;
+        }
+    }
+}
